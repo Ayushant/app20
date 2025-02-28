db818bb631ef0540333c27e7116d8b38bb9cbd0e
Mayank Bansal <mayank.bansal.0527@gmail.com>
2025-02-28 17:04:09 +0530

    cors package

--- server/server.js
+++ server/server.js
@@ -3,8 +3,12 @@ const mongoose = require('mongoose');
 const connectdb = require("./config/db")
 // const buyerRoutes = require('./routes/buyer')
 const path = require('path');
+const cors = require('cors');
+
 
 const app = express()
+
+app.use(cors());
 app.use(express.json())
 const sellerRoutes = require('./routes/seller')
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
