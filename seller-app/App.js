0ce3b8ce63e966b032319d3b50135a6a664f366e
Mayank Bansal <mayank.bansal.0527@gmail.com>
2025-03-01 16:02:45 +0530

    upload product at app.js

--- seller-app/App.js
+++ seller-app/App.js
@@ -4,19 +4,20 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen.js';
 import RegisterScreen from './screens/RegisterScreen.js';
 import LoginScreen from './screens/LoginScreen.js';
-// import UploadProductScreen from './screens/UploadProductScreen.js';
-// import OrdersScreen from './screens/OrdersScreen.js';
+import UploadProductScreen from './screens/UploadProductScreen.js';
+import ProductListScreen from './screens/ProductListScreen.js';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="UploadProduct" component={UploadProductScreen} />
+        <Stack.Screen name="ProductList" component={ProductListScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
