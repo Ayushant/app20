40dc1f4820750858a7e42fb2e3dc6adf75de7461
Mayank Bansal <mayank.bansal.0527@gmail.com>
2025-03-31 11:45:27 +0530

    syntax corrected

--- server/routes/buyer.js
+++ server/routes/buyer.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const auth = require('../middleware/auth');
+const {auth} = require('../middleware/auth');
 const {
     sendOTP,
     verifyOTP,
